45404d20da3bf9a45d2d79bbe683a677459a7ce0
Risto Ikonen <ristoikonen@gmail.com>
2025-04-15 10:49:01 +1000

    Model Context Protocol (MCP) server

---
+++ EchoTool.cs
@@ -0,0 +1,20 @@
+﻿using ModelContextProtocol.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MC
+{
+    [McpServerToolType]
+    public static class EchoTool
+    {
+        [McpServerTool, Description("Echoes the message back to the client.")]
+        public static string Echo(string message) => $"Hello from C#: {message}";
+
+        [McpServerTool, Description("Echoes in reverse the message sent by the client.")]
+        public static string ReverseEcho(string message) => new string(message.Reverse().ToArray());
+    }
+}
---
+++ MonkeyTools.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel;
+using System.Text.Json;
+
+using ModelContextProtocol.Server;
+
+namespace MC;
+
+[McpServerToolType]
+public static class MonkeyTools
+{
+    [McpServerTool, Description("Get a list of monkeys.")]
+    public static async Task<string> GetMonkeys(MonkeyService monkeyService)
+    {
+        var monkeys = await monkeyService.GetMonkeys();
+        return JsonSerializer.Serialize(monkeys);
+    }
+
+    [McpServerTool, Description("Get a monkey by name.")]
+    public static async Task<string> GetMonkey(MonkeyService monkeyService, [Description("The name of the monkey to get details for")] string name)
+    {
+        var monkey = await monkeyService.GetMonkey(name);
+        return JsonSerializer.Serialize(monkey);
+    }
+}
